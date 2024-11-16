a8ff9be70dd0d95a94ff1849cbacc2031eac3ec3
Diti-Jain <ditijainj@gmail.com>
2024-11-17 02:49:00 +0530

    Added: src/App.jsx, src/assets/image1.png, src/assets/image2.png, src/components/homepage.css, src/components/homepage.jsx; Modified: package-lock.json, package.json; Deleted: src/App.js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/homepage.jsx';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/homepage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './homepage.css';
+import logo from '../assets/image1.png';
+
+const App = () => {
+    const goToNextPage = () => {
+        window.location.href = "fileupload.html"; // Update URL as needed
+    };
+
+    return (
+        <div className="App">
+            <div className="top-bar">
+                <div className="logo">LastMin</div>
+                <button onClick={goToNextPage}>Get Started</button>
+            </div>
+            <div className="content-wrapper">
+                <div className="container">
+                    <div className="text-content">
+                        <h1>LastMin</h1>
+                        <h2>Your Last Minute Tutor</h2>
+                        <p>
+                            Turn PDFs and videos into bite-sized insights and get answers to your
+                            questions with just a click.
+                        </p>
+                        <button onClick={goToNextPage}>Get Started</button>
+                    </div>
+                    <div className="image-container">
+                        <img src={logo} alt="Tutoring Image" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
