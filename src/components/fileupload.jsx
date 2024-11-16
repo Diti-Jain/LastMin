89a08136192b29731b66013ac73b0c64966bbe87
Diti-Jain <ditijainj@gmail.com>
2024-11-17 02:50:46 +0530

    Added: src/components/fileupload.css, src/components/fileupload.jsx

---
+++ src/components/fileupload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoUploadPage from './VideoUploadPage';
+
+function App() {
+  return (
+    <div>
+      <VideoUploadPage />
+    </div>
+  );
+}
+
+export default App;
